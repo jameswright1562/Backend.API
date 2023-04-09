faf3807cdb488617d038c385139a55e9770cfd62
jameswright1563 <45405619+jameswright1563@users.noreply.github.com>
2023-04-09 14:27:44 +0100

    Add project files.

---
+++ Servicing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using CloudEventData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Net.Http.Headers;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
+using System.Runtime.CompilerServices;
+using Backend.Model;
+using Backend.Interfaces;
+
+namespace Backend.API;
+
+[ApiController]
+public class Servicing
+{
+    private readonly Settings _settings;
+    private readonly IValidator _validator;
+    public Servicing(IConfiguration config, IValidator validator)
+    {
+        _settings = config.Get<Settings>();
+        _validator = validator;
+    }
+
+    [FunctionName("GetPatientData")]
+    [OpenApiOperation(operationId: "GetPatientData", tags: new[] { "reads" })]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiParameter(name: "idOfPatient", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "the id of the requested price")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/json", bodyType: typeof(JObject), Description = "OK response")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.NotFound, contentType: "text/plain", bodyType: typeof(string), Description = "NOT FOUND response")]
+    public async Task<IActionResult> GetPatientData(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "prices/{id}")]
+        HttpRequest req, string id)
+    {
+        if (!_validator.ValidatePatientDataRequest(req, id).Result)
+            return new UnauthorizedResult();
+        try
+        {
+            //Get data from database and return
+            return new ObjectResult(null);
+        }
+        catch (Exception ex)
+        {
+            return new NotFoundObjectResult(ex.GetBaseException().Message);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Backend.Interfaces;
+using Backend.Tokens;
+
+
+[assembly: FunctionsStartup(typeof(Backend.API.Startup))]
+namespace Backend.API;
+
+public class Startup : FunctionsStartup
+{
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder config) 
+    {
+        var context = config.GetContext();
+        var appPath = context.ApplicationRootPath;
+
+        config.ConfigurationBuilder
+            .AddJsonFile(Path.Combine(appPath, "settings.json"), optional: true, reloadOnChange: true)
+            .AddJsonFile(Path.Combine(appPath, "local.settings.json"), optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+    }
+        
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddScoped<IValidator, Validator>();
+        builder.Services.AddHttpClient();
+    }
+}
